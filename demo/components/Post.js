e45d701529f1f66ac585b5fe34ac182eb03880a9
Andrea Carraro <me@andreacarraro.it>
2020-10-11 23:24:32 +0200

    Fine tune demo to deploy on GH pages

--- demo/components/Post.js
+++ demo/components/Post.js
@@ -4,10 +4,11 @@ import PostGrid from './PostGrid';
 const Post = ({ id, pathname, pageState, showPostGrid }) => {
   return (
     <div className={styles.post}>
-      <p>
-        I am the post {id}; my pathname is: {pathname}
-      </p>
-      {pageState && <p>Page state is: {pageState}</p>}
+      <h2>I am the post {id}</h2>
+      <h3>my pathname is: {pathname}</h3>
+      {pageState && (
+        <h3>Initial page state is preserved and is: {pageState}</h3>
+      )}
       {showPostGrid && (
         <>
           <p>Go to another post staying in contextual navigation:</p>
