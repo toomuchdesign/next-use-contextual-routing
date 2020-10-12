c9cb3c3e0839a02e6ff0fbcaf9c7e3b86393f702
Andrea Carraro <me@andreacarraro.it>
2020-10-12 16:15:28 +0200

    Abstract demo style

--- demo/components/InitialStateGrid.js
+++ demo/components/InitialStateGrid.js
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-import styles from './styles.module.css';
+import Grid from './Grid';
+import SquaredLink from './SquaredLink';
 import { backgroundColors } from 'demo/pages/index';
 
 const InitialStateGrid = () => {
   return (
-    <div className={styles.postCardGridWrapper}>
-      <div className={styles.postCardGrid}>
-        {Object.entries(backgroundColors).map(([id, color], index) => (
-          <Link key={index} href={`/?state=${id}`}>
-            <a className={styles.postCard} style={{ backgroundColor: color }}>
-              Initial State: {id}
-            </a>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <Grid>
+      {Object.entries(backgroundColors).map(([id, color], index) => (
+        <SquaredLink
+          key={index}
+          href={`/?state=${id}`}
+          style={{ backgroundColor: color }}
+        >
+          Initial State: {id}
+        </SquaredLink>
+      ))}
+    </Grid>
   );
 };
 
--- demo/components/PostGrid.js
+++ demo/components/PostGrid.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import styles from './styles.module.css';
+import Grid from './Grid';
+import SquaredLink from './SquaredLink';
 import { useContextualRouting } from 'src';
 export const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -7,30 +7,16 @@ export default function PostGrid({ small }) {
   const { makeContextualHref } = useContextualRouting();
 
   return (
-    <div className={styles.postCardGridWrapper}>
-      <div
-        className={[
-          styles.postCardGrid,
-          small && styles.postCardGrid__small,
-        ].join(' ')}
-      >
-        {data.map((id, index) => (
-          <Link
-            key={index}
-            href={makeContextualHref({ postId: id })}
-            as={`/post/${id}`}
-          >
-            <a
-              className={[
-                styles.postCard,
-                small && styles.postCard__small,
-              ].join(' ')}
-            >
-              {id}
-            </a>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <Grid small={small}>
+      {data.map((id, index) => (
+        <SquaredLink
+          key={index}
+          href={makeContextualHref({ postId: id })}
+          as={`/post/${id}`}
+        >
+          {id}
+        </SquaredLink>
+      ))}
+    </Grid>
   );
 }
